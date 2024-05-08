4dc99e5213586b3e9c83e8735aa150446a3c53f0
Marek Koprek <245847@edu.p.lodz.pl>
2024-05-08 13:10:11 +0200

    Adding base files for calendar and events and linking them on navigation bar

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/', views.profileHome, name='profile-home')
+    path('profile/', views.profileHome, name='profile-home'),
+    path('events/', views.eventsSearch, name='events-search'),
+    path('calendar/', views.calendar, name='calendar')
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def profileHome(request):
     return render(request, 'main/profile_home.html')
+
+def eventsSearch(request):
+    return render(request, "main/events_search.html")
+
+def calendar(request):
+    return render(request, "main/calendar.html")
