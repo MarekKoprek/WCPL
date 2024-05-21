3ae8f1bce6ae7ac53762ac4c2dc3f04f0309c505
Roman Beznosenko <245125@edu.p.lodz.pl>
2024-05-21 19:30:17 +0200

    Profile page update

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'phone_number',
+        'faculty',
+        'course',
+        'semester',
+        'bio'
+    )
+
+admin.site.register(Profile, ProfileAdmin)
