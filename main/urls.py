70adf94f2ae714bafce46423f7d68c0e2e8dac1c
Marek Koprek <245847@edu.p.lodz.pl>
2024-05-21 23:02:11 +0200

    Adding event-search interface

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('profile/', views.profileHome, name='profile-home'),
     path('events/', views.eventsSearch, name='events-search'),
-    path('calendar/', views.calendar, name='calendar')
+    path('calendar/', views.calendar, name='calendar'),
+    path('events/add', views.eventsAdd, name='events-add')
 ]
--- main/views.py
+++ main/views.py
@@ -3,8 +3,55 @@ from django.shortcuts import render
 def profileHome(request):
     return render(request, 'main/profile_home.html')
 
+events = [
+    {
+        'title': 'Tytuł ogłoszenia',
+        'description': 'Ogólny opis wydarzenia cokolwiek tutaj może być.',
+        'date': '25.12.2023'
+    },
+    {
+        'title': 'Tytuł ogłoszenia',
+        'description': 'Ogólny opis wydarzenia cokolwiek tutaj może być.',
+        'date': '26.12.2023'
+    },
+    {
+        'title': 'Tytuł ogłoszenia',
+        'description': 'Ogólny opis wydarzenia cokolwiek tutaj może być.',
+        'date': '27.12.2023'
+    },
+    {
+        'title': 'Tytuł ogłoszenia',
+        'description': 'Ogólny opis wydarzenia cokolwiek tutaj może być.',
+        'date': '28.12.2023'
+    },
+    {
+        'title': 'Tytuł ogłoszenia',
+        'description': 'Ogólny opis wydarzenia cokolwiek tutaj może być.',
+        'date': '29.12.2023'
+    },
+    {
+        'title': 'Tytuł ogłoszenia',
+        'description': 'Ogólny opis wydarzenia cokolwiek tutaj może być.',
+        'date': '30.12.2023'
+    },
+    {
+        'title': 'Tytuł ogłoszenia',
+        'description': 'Ogólny opis wydarzenia cokolwiek tutaj może być.',
+        'date': '31.12.2023'
+    }
+]
+
+participantRows = [1, 2, 3, 4]
+
 def eventsSearch(request):
-    return render(request, "main/events_search.html")
+    context = {
+        'events': events,
+        'participantRows': participantRows
+        }
+    return render(request, "main/events_search.html", context)
+
+def eventsAdd(request):
+    return render(request, "main/events_add.html")
 
 def calendar(request):
     return render(request, "main/calendar.html")
