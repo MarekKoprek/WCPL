99ff67d9f541114148911c271fd315fe762e0804
o245770 <245770@edu.p.lodz.pl>
2024-05-26 04:30:35 +0200

    Added new fields in register form

--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import  messages
+from .forms import UserRegisterForm
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.firstName = form.cleaned_data.get('firstName')
+            user.lastName = form.cleaned_data.get('lastName')
+            username = form.cleaned_data.get('username')
+
+            messages.success(request, f'Konto {username} utworzone')
+            return redirect('events-search')
+    else:
+        form = UserRegisterForm()
+
     return render(request, 'users/register.html', {'form': form})
