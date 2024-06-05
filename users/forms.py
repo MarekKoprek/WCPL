b2c48a66ef7ae66b6dce5dd1f68b4ecc04a94539
o245770 <245770@edu.p.lodz.pl>
2024-06-05 11:14:22 +0200

    Modified registration form

--- users/forms.py
+++ users/forms.py
@@ -3,18 +3,19 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class UserRegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
+    TYPE = [
+        ('1', 'Student account'),
+        ('2', 'Firm account'),
+    ]
     email = forms.EmailField()
-    phone_number = forms.CharField(max_length=9, required=True)
-    department = forms.CharField(max_length=10, required=True)
-    major = forms.CharField(max_length=15, required=True)
-    semester = forms.IntegerField(required=True)
-    about_me = forms.CharField(max_length=200, required=False)
+    type = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        choices=TYPE,
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'phone_number', 'department', 'major', 'semester', 'about_me', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
