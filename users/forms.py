c8ba74587fdca79ce3707a67795f56c93bb245bb
o245770 <245770@edu.p.lodz.pl>
2024-06-12 11:26:29 +0200

    Changes to forms

--- users/forms.py
+++ users/forms.py
@@ -5,11 +5,19 @@ from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    USER_TYPE_CHOICES = (
+        ('student', 'Student'),
+        ('firm', 'Firm'),
+    )
+    user_type = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        choices=USER_TYPE_CHOICES,
+    )
 
 
     class Meta:
         model = User
-        fields = ['username', 'email','password1', 'password2']
+        fields = ['username', 'email','password1', 'password2', 'user_type']
 
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
